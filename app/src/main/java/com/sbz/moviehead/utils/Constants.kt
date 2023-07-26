30e474a18d814d8f45fea6e2db873c52e3fffb4f
SHabaj-dev <shabajansari843@gmail.com>
2023-07-27 00:11:05 +0530

    Initial comit

--- app/src/main/java/com/sbz/moviehead/utils/Constants.kt
+++ app/src/main/java/com/sbz/moviehead/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.sbz.moviehead.utils
 
 object Constants {
     const val DELAY_TIME = 2000L
-    const val API_KEY = "6621ef4429457083a4f3475925fbd9f4"
+    const val API_KEY = "your Api key goes here"
     const val BASEURL = "https://api.themoviedb.org/3/"
     const val POSTER_URL = "https://image.tmdb.org/t/p/w342"
 }
